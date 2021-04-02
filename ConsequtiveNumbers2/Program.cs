781800e7fc67296438746fcb4c1101059133fb8d
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-04-02 20:35:38 +0100

    Update Program.cs

--- ConsequtiveNumbers2/Program.cs
+++ ConsequtiveNumbers2/Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 
 namespace ConsequtiveNumbers2
 {
     class Program
     {
+        // https://edabit.com/challenge/ywHrStmf93Thm3x3B
+        // UNTESTED/UNFINISHED
         static void Main(string[] args)
         {
             ConseqCalc calc = new(new int[] { 1, 3, 5, 5, 3, 3, 1 }, 3, 2);
