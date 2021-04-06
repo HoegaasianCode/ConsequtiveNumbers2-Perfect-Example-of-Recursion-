e4e5a8b3884fb75d97203e8bcb09268c3957505d
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-04-06 15:42:38 +0200

    Update ConseqCalc.cs

--- ConsequtiveNumbers2/ConseqCalc.cs
+++ ConsequtiveNumbers2/ConseqCalc.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace ConsequtiveNumbers2
 {
     public class ConseqCalc
@@ -7,52 +5,35 @@ namespace ConsequtiveNumbers2
         private readonly int[] _numbers;
         private readonly int _n;
         private readonly int _tresholdCount;
-        private List<int> ShiftedInts { get; set; }
         private int ConsequtiveCount { get; set; }
-        private bool IsConsequtive { get; set; }
+        private bool IsConsequtiveNumbers { get; set; }
 
         public ConseqCalc(int[] numbers, int n, int tresholdCount) //  { 1, 3, 5, 5, 3, 3 }, 3, 3);
         {
             _numbers = numbers;
             _n = n;
             _tresholdCount = tresholdCount;
-            ShiftedInts = new();
-            ConsequtiveCount = 1;
-            IsConsequtive = false;
+            IsConsequtiveNumbers = false;
         }
 
-        public void IsConsequtiveOrder()
+        public void IsConsequtive()
         {
-            for(int i = 0; i < _numbers.Length; i++)
+            for (int i = 0; i < _numbers.Length; i++)
             {
                 int number = _numbers[i];
-                int shiftedInt = ShiftedInts[i];
-                if (number == shiftedInt && number == _n)
+                if (number != _n)
                 {
-                    ConsequtiveCount++;
-                    ShiftIntsByN();
+                    ConsequtiveCount = 0; // reset count
+                    continue; // finally... a purpose for 'continue' (skips 2nd check)
                 }
+                ConsequtiveCount++;
                 if (ConsequtiveCount == _tresholdCount)
                 {
-                    IsConsequtive = true;
-                    break;
+                    IsConsequtiveNumbers = true;
                 }
             }
         }
 
-        public void ShiftIntsByN()
-        {
-            int offset = -1;
-            List<int> shiftedInts = new();
-            for(int i = ConsequtiveCount; i < _numbers.Length; i++)
-            {
-                int number = _numbers[i];
-                shiftedInts.Add(number);
-                shiftedInts.Add(offset);
-            }
-            ShiftedInts = shiftedInts;
-        }
-
-        public bool IsValid() => IsConsequtive;
+        public bool IsValid() => IsConsequtiveNumbers;
     }
 }
